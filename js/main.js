2796500bf4a0336ae5d028bbc87e09db6ee5b0ab
szymtur <szymtur@gmail.com>
2018-09-29 13:40:53 +0200

    ie11_compatibility

--- js/main.js
+++ js/main.js
@@ -17,6 +17,8 @@ const initialData = [
     {"male_percent": 44}
 ];
 
+// JSON.stringify(obj) — converts an JavaScript object to a JSON string
+// JSON.parse(str) — converts a JSON string back to a JavaScript object
 const inputData = JSON.parse(JSON.stringify(initialData));
 
 
@@ -26,7 +28,8 @@ function postData(data) {
         url: "https://api.myjson.com/bins/",
         method: "POST",
         dataType: "json",
-        contentType: "application/json",
+        contentType: "application/json; charset=utf-8",
+        cache: false,
         data: JSON.stringify(data)
     }).done(function (response) {
         console.log(response.uri);
@@ -51,7 +54,8 @@ function getData(url) {
     $.ajax({
         method: "GET",
         url: url,
-        dataType: "json"
+        dataType: "json",
+        cache: false
     }).done(function (response) {
         console.log('connected');
         checkingArrows(response[6]['gdp']);
@@ -83,83 +87,81 @@ function putData(data) {
 
 //changing data
 function changeData(data) {
-    let array = ['-', '+'];
-    let randomSign = array[Math.floor(Math.random() * array.length)];
-    let randomIndex = Math.floor(Math.random() * data.length-4);     
+    const array = ['-', '+'];
+    const randomSign = array[Math.floor(Math.random() * array.length)];
+    const randomIndex = Math.floor(Math.random() * data.length - 4);
 
     //random changing headcount and spending data
-    for(let key in data[randomIndex]) {
-        let newData = eval(`${data[randomIndex][key]} ${randomSign} ${data[randomIndex][key]*0.05}`).toFixed(2);
+    for (let key in data[randomIndex]) {
+        let newData = eval(data[randomIndex][key] + ' ' + randomSign + ' ' + data[randomIndex][key] * 0.05).toFixed(2);
         data[randomIndex][key] = Number(newData);
     }
     //changing gender_percent
-    for(let key in data[data.length-1]) {
-        let newData = Math.round(eval(`${data[data.length-1][key]} ${randomSign} ${data[data.length-1][key]*0.03}`));
-        data[data.length-1][key] = Number(newData);
+    for (let _key in data[data.length - 1]) {
+        let _newData = Math.round(eval(data[data.length - 1][_key] + ' ' + randomSign + ' ' + data[data.length - 1][_key] * 0.03));
+        data[data.length - 1][_key] = Number(_newData);
     }
     //changing gdp_percent
-    for(let key in data[data.length-2]) {
-        let newData = Math.round(eval(`${data[data.length-2][key]} ${randomSign} 1`));
-        data[data.length-2][key] = Number(newData);
+    for (let _key2 in data[data.length - 2]) {
+        let _newData2 = Math.round(eval(data[data.length - 2][_key2] + ' ' + randomSign + ' 1'));
+        data[data.length - 2][_key2] = Number(_newData2);
     }
     //changing gdp_spending
-    for(let key in data[data.length-3]) {
-        if(randomSign == '-'){
-            data[data.length-3][key] -= 2;
-        }
-        else if(randomSign == '+'){
-            data[data.length-3][key] += 2;
+    for (let _key3 in data[data.length - 3]) {
+        if (randomSign == '-') {
+            data[data.length - 3][_key3] -= 2;
+        } else if (randomSign == '+') {
+            data[data.length - 3][_key3] += 2;
         }
     }
     //changing gdp_headcount
-    for(let key in data[data.length-4]) {
-        if(randomSign == '-'){
-            data[data.length-4][key] -= 1;
-        }
-        else if(randomSign == '+'){
-            data[data.length-4][key] += 1;
+    for (let _key4 in data[data.length - 4]) {
+        if (randomSign == '-') {
+            data[data.length - 4][_key4] -= 1;
+        } else if (randomSign == '+') {
+            data[data.length - 4][_key4] += 1;
         }
     }
     return data;
 }
 
 
-//chart rendering
-function chartRender(inputData){
-    const width = 100;
-    const height = 100;
-    const outerRadius = width/2;
-    const innerRadius = width/3.5 ;
+// //chart rendering
+function chartRender(inputData) {
+    let width = 100;
+    let height = 100;
+    let outerRadius = width / 2;
+    let innerRadius = width / 3.5;
 
     const data = [
-        { start: 0, stop: 100-inputData, color: "#0bd8aa"},
-        { start: 100-inputData, stop: 100, color: "#53a8e2"}
+        { start: 0, stop: 100 - inputData, color: "#0bd8aa" }, 
+        { start: 100 - inputData, stop: 100, color: "#53a8e2" }
     ];
 
-    const myScale = d3.scale
+    let myScale = d3.scale
         .linear()
         .domain([0, 100])
         .range([0, 2 * Math.PI]);
 
-    const svg = d3.select("div.svg-container")
+    let svg = d3.select("div.svg-container")
         .append("svg")
         .attr("preserveAspectRatio", "xMinYMin meet")
-        .attr("viewBox", `0 0 ${width} ${height}`)
+        .attr("viewBox", "0 0 " + width + " " + height)
         .classed("svg-content", true);
 
-    const arc = d3.svg.arc()
+    let arc = d3.svg.arc()
         .innerRadius(innerRadius)
         .outerRadius(outerRadius)
-        .startAngle(function(d){return myScale(d.start)})
-        .endAngle(function(d){return myScale(d.stop)});
+        .startAngle(function(d){return myScale(d.start);})
+        .endAngle(function(d){return myScale(d.stop);});
 
     svg.selectAll("path")
         .data(data)
         .enter()
         .append("path")
         .attr("d", arc)
-        .style("fill", function(d){return d.color})
-        .attr("transform", `translate(${width/2}, ${height/2})`);
+        .style("fill", function(d){return d.color;})
+        .attr("transform", "translate(" + width / 2 + ", " + height / 2 + ")");
 }
 
 
@@ -177,16 +179,16 @@ function setMarketsize(data){
     let spending_share = top_container.find('.spending_share').text(data_spending_share);
     
     let data_male_percent = data[7].male_percent;
-    let data_gdp = (data[6].gdp) <= 0 ? data[6].gdp : `+${data[6].gdp}`;
+    let data_gdp = data[6].gdp <= 0 ? data[6].gdp : "+" + data[6].gdp;
     let data_gdp_headcount = Math.round(data[4].gdp_headcount);
     let data_gdp_spending = Math.round(data[5].gdp_spending);
 
     let bottom_container = $('.bottom_container');
-    let male_percent = bottom_container.find('.male').text(`${data_male_percent}%`);
-    let female_percent = bottom_container.find('.female').text(`${100-data_male_percent}%`);
-    let gdp = bottom_container.find('.gdp_percent').text(`${data_gdp}%`);
-    let gdp_headcount = bottom_container.find('.gdp_headcount').text(`${data_gdp_headcount}%`);
-    let gdp_spending = bottom_container.find('.gdp_spending').text(`${data_gdp_spending}%`);
+    let male_percent = bottom_container.find('.male').text(data_male_percent + '%');
+    let female_percent = bottom_container.find('.female').text(100 - data_male_percent + '%');
+    let gdp = bottom_container.find('.gdp_percent').text(data_gdp + '%');
+    let gdp_headcount = bottom_container.find('.gdp_headcount').text(data_gdp_headcount + '%');
+    let gdp_spending = bottom_container.find('.gdp_spending').text(data_gdp_spending + '%');
 
     function numberWithCommas(value) {
         return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
---
+++ readme.md
@@ -0,0 +1,45 @@
+![](img/preview.png)
+
+### CSS
+
+- Korzystamy tylko i wyłącznie z jednostek miary VW oraz (VH w pewnych przypadkach).
+
+- W przypadku tego zadania nie ma potrzeby wykorzystywania VH.
+    Przykład:
+
+    ```html
+	h1 {
+		font-size:1.2vw
+		left:5vw 
+		line-height:0.8vw 
+	}
+    ```
+- Wyjątkiem są cienkie linie je można zapisać jako 1px.
+
+- Korzystamy tylko i wyłącznie z czcionki 'Lato'.
+
+- Kolory muszą być 1:1 jak na obrazku
+
+### JavaScript
+
+- Piszemy kod w czystym JS z pomocą jQuery
+
+- Do stworzenia wykresu kołowego korzystamy z biblioteki do proceduralnego generowania SVG - D3.js
+
+- Ogólna funkcja generująca wykres powinna zawierać jeden argument, który odpowiada procentowi wypełnienia niebieskiej części.
+
+- Cały moduł powinien mieć funkcje ustawiającą jego zawartość:
+
+    ```javascript
+	 setMarketsize (
+		headcount,
+		headcount_share,
+		spending,
+		spending_share,
+		gender_percent,
+		gdp,
+		gdp_headcount,
+		gdp_spending
+	)
+    ```
+- Argument __gender_percent__ jest wykorzystywany w funkcji ogólnej generującej wykres kołowy.
